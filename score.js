530512af8e955c0797c897ab5a8c60a7c6ca7e29
manisha ratlavath <manisharatlavath@gmail.com>
2024-03-19 12:08:39 +0530

    added again option  feature in js file

--- score.js
+++ score.js
@@ -6,25 +6,33 @@ let sco=document.getElementById('score');
 let hsco=document.getElementById('hscore');
 let b=document.getElementById('by');
 let n=document.getElementById('num');
+let p_again=document.getElementById('p_again');
 let hs=0;
-let c=1;
+let s=20;
 usub.addEventListener('click',()=>{
     if(uinp.value==v){
         n.innerText=v;
         gues.innerText="perfect";
-        let s=20-c;
         sco.innerText="Score:"+s;
         if(hs<s){
             hs=s;
-            console.log(hs);
             hsco.innerText="High Score:"+hs;
         }
         b.style.backgroundColor='#FF99FF';
     }else if(uinp.value<v){
-        c++;
         gues.innerText="lesser";
+        sco.innerText="Score:"+--s;
     }else if(uinp.value>v){
-        c++;
         gues.innerText="greater";
+        sco.innerText="Score:"+--s;
     }
-});
+})
+p_again.addEventListener('click',()=>{
+    s=20;
+    v=Math.floor(Math.random()*20)+1;
+    n.innerText='?';
+    uinp.value="";
+    gues.innerText="Start Guessing";
+    sco.innerText="Score:20";
+    b.style.backgroundColor='black';
+})
